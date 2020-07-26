fd6ed5e40dc8fe1f4c5c298c7b72d0b5a192a1f2
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-07-26 15:37:08 +0300

    add basic structure

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
