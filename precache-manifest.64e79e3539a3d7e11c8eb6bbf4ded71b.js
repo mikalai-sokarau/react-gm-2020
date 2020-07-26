f352b1bd868b421cc7a1dffbfb089852a82a4b2a
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-07-26 23:48:22 +0300

    Updates

---
+++ precache-manifest.64e79e3539a3d7e11c8eb6bbf4ded71b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e62d340b5d137a0b76ae39bd5574bc",
+    "url": "/react-gm-2020/index.html"
+  },
+  {
+    "revision": "42dd22cf76ebacf89384",
+    "url": "/react-gm-2020/static/css/main.5471afbd.chunk.css"
+  },
+  {
+    "revision": "b474f889ae7d102667fe",
+    "url": "/react-gm-2020/static/js/2.62e903d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gm-2020/static/js/2.62e903d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42dd22cf76ebacf89384",
+    "url": "/react-gm-2020/static/js/main.fee48666.chunk.js"
+  },
+  {
+    "revision": "61038355b9d6cdc659b5",
+    "url": "/react-gm-2020/static/js/runtime-main.457af1df.js"
+  }
+]);
