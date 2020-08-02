dc783290f935b49ba1af7a5d02dccc34b5d57fe6
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-08-02 21:21:42 +0300

    add index files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.component";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
