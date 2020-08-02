5d461816d467b98598eee5206b7e2a2c857e38fa
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-08-03 00:02:45 +0300

    index files refactoring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app/app.component";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app.component';
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
