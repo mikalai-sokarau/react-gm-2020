d58e6e16feafa746c6ed95dddb1fcd1076e34005
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-08-02 21:21:22 +0300

    add app component

---
+++ src/components/app/app.component.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import s from './app.component.css';
+
+export default class MyComponent extends Component {
+  render() {
+    return <div className={s.core}>Hello World</div>;
+  }
+}
