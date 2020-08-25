b5089ced1564667603687e0b303ad2bfd72564ee
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-08-25 14:57:45 +0300

    normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@app/components/app/app.component';
+import 'index.css'; // TODO: import styles directly into index.html using webpack
 
 ReactDOM.render(<App />, document.querySelector('#root'));
