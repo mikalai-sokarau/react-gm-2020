cbcb1492461a65e39a8ce8e5fc48b05f28a7a544
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-08-30 11:32:30 +0300

    add button component

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cN from '@app/utils/utils';
+import style from '@app/components/button/button.component.style';
+import { IButton, ButtonType, BUTTON_TEXT } from '@app/components/button/button.interface';
+
+const Button = ({ type, callback }: IButton): JSX.Element => {
+  const { button, ...classes } = style();
+  const buttonClassName = ButtonType[type];
+
+  return (
+    <input
+      type="button"
+      className={cN(button, classes[buttonClassName])}
+      value={BUTTON_TEXT[type]}
+      onClick={callback}
+    />
+  );
+};
+
+export default Button;
