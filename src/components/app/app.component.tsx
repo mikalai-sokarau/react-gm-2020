f84cd6547aa807c6eca0b4940f568d65c3f44995
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-09-06 21:24:02 +0300

    add ErrorBoundary component

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -3,15 +3,18 @@ import style from '@app/components/app/app.component.styles';
 import Main from '@app/components/main/main.component';
 import Header from '@app/components/header/header.component';
 import Footer from '@app/components/footer/footer.component';
+import ErrorBoundary from '@app/components/errorBoundary/errorBoundary.component';
 
 const App = (): JSX.Element => {
   const classes = style();
 
   return (
     <div className={classes.core}>
-      <Header />
-      <Main />
-      <Footer />
+      <ErrorBoundary>
+        <Header />
+        <Main />
+        <Footer />
+      </ErrorBoundary>
     </div>
   );
 };
