c5fca559fc90834368f8298acb3e33d0020d95a2
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-10-01 00:02:25 +0300

    init cors

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,14 @@
 import Koa from 'koa';
+import cors from '@koa/cors';
 import router from './routes';
 import KoaJson from 'koa-json';
+
+// TODO: add absolute paths support to server
 
 const app = new Koa();
 const port = process.env.NODE_PORT;
 
+app.use(cors());
 app.use(KoaJson());
 app.use(router.routes());
 app.use(router.allowedMethods());
