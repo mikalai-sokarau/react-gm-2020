e61453cd802551a66c91252e81b3ef94114e31a3
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-10-03 12:18:40 +0300

    add koa bodyparser

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import Koa from 'koa';
 import cors from '@koa/cors';
 import router from './routes';
 import KoaJson from 'koa-json';
+import BodyParser from 'koa-bodyparser';
 
 // TODO: add absolute paths support to server
 
@@ -10,6 +11,7 @@ const port = process.env.NODE_PORT;
 
 app.use(cors());
 app.use(KoaJson());
+app.use(BodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
