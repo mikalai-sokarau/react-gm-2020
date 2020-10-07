4253120a12bb7856b0ea8df7c3ea7713f180ee13
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-10-08 01:44:57 +0300

    add lazy loading

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,16 +1,19 @@
+import React, {
+  useState, FC, lazy, Suspense,
+} from 'react';
 import store from '@app/store/store';
-import React, { useState, FC } from 'react';
 import { StoreContext } from 'storeon/react';
-import Main from '@app/components/main/main.component';
 import { IMovie } from '@shared/interfaces/movies.model';
-import Header from '@app/components/header/header.component';
-import Footer from '@app/components/footer/footer.component';
 import useStyle from '@app/components/app/app.component.styles';
 import CoreModal from '@app/components/modals/coreModal/coreModal.component';
 import MovieDetails from '@app/components/movieDetails/movieDetails.component';
 import HeaderActiveComponent from '@app/components/app/app.component.interface';
 import ErrorBoundary from '@app/components/errorBoundary/errorBoundary.component';
 import { IModalContext, ModalContext } from '@shared/interfaces/coreModal.context';
+
+const Main = lazy(() => import('@app/components/main/main.component'));
+const Header = lazy(() => import('@app/components/header/header.component'));
+const Footer = lazy(() => import('@app/components/footer/footer.component'));
 
 const App: FC = () => {
   const classes = useStyle();
@@ -33,17 +36,20 @@ const App: FC = () => {
       <ErrorBoundary>
         <StoreContext.Provider value={store}>
           <ModalContext.Provider value={{ chosenModal, setChosenModal }}>
-            {headerComponent === HeaderActiveComponent.Header
-              ? <Header />
-              : (
-                <MovieDetails
-                  movie={chosenMovie}
-                  onBackButtonClick={() => toggleHeaderComponent(HeaderActiveComponent.Header)}
-                />
-              )}
-            <Main onMovieImageClick={handleMovieClick} />
-            <Footer />
-            {chosenModal.type && <CoreModal />}
+            <Suspense fallback={<div>Loading...</div>}>
+              {headerComponent === HeaderActiveComponent.Header
+                ? <Header />
+                : (
+                  <MovieDetails
+                    movie={chosenMovie}
+                    onBackButtonClick={() => toggleHeaderComponent(HeaderActiveComponent.Header)}
+                  />
+                )}
+
+              <Main onMovieImageClick={handleMovieClick} />
+              <Footer />
+              {chosenModal.type && <CoreModal />}
+            </Suspense>
           </ModalContext.Provider>
         </StoreContext.Provider>
       </ErrorBoundary>
