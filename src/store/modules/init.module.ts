a5898fa849603bee2941e8926f8f97aee76cfeaf
Mikalai Sokarau <mikalai_sokarau@epam.com>
2020-10-20 22:31:12 +0300

    remove initial movies request

--- src/store/modules/init.module.ts
+++ src/store/modules/init.module.ts
@@ -4,14 +4,10 @@ import {
 } from '@app/store/store.interface';
 
 const initModule: StoreonModule<IState, IEvents> = (store) => {
-  store.on('@init', () => {
-    store.dispatch(ActionType.getMovies, DEFAULT_SEARCH_STATE);
-
-    return {
-      movies: [],
-      search: DEFAULT_SEARCH_STATE,
-    };
-  });
+  store.on('@init', () => ({
+    movies: [],
+    search: DEFAULT_SEARCH_STATE,
+  }));
 };
 
 export default initModule;
